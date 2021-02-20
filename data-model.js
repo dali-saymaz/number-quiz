658f053970d6b39dcef8d63864a4feb5c9cf28e3
unknown <dalidasy24@gmail.com>
2021-02-20 01:08:03 +0100

    localstorage

---
+++ data-model.js
@@ -0,0 +1,27 @@
+
+let questions = [
+  {
+    question: "1*5 bu iki rakamin carpimi nedir",
+    choice1: "5",
+    choice2: "67",
+    choice3: "77",
+    choice4: "10",
+    answer: 1
+  },
+  {
+    question: "2*5 bu iki rakamin carpimi nedir",
+    choice1: "5",
+    choice2: "67",
+    choice3: "10",
+    choice4: "10",
+    answer: 3
+  },
+  {
+    question: "3*5 bu iki rakamin carpimi nedir",
+    choice1: "5",
+    choice2: "67",
+    choice3: "77",
+    choice4: "15",
+    answer: 4
+  }
+];
--- game.js
+++ game.js
@@ -1,90 +1,96 @@
 const question = document.getElementById("question");
+// Array.from() secenekleri bir diziye donusturmek icin kullandim // burada 4 ayri sik elde etmis oluyoruz
+// https://developer.mozilla.org/tr/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+//const questionCounterText = document.getElementById("questionCounter");//soru gostergesi//2-2 silinen
+const progressText = document.getElementById("progressText");//progres ust yazi//2-3
+const progressBarFull = document.getElementById("progressBarFull");//soru gostergesi//3-3
+const scoreText = document.getElementById("score");//soru score gostergesi 2-1
 
-let currentQuestion = {};
+
+
+let currentQuestion = {};//ekrana yazilan soru her sorudan sonra gelen yeni soruyu temsil eder obje kullandik soru ve 4 sikki var
 let acceptingAnswers = false;
-let score = 0;
+let score = 0;//
 let questionCounter = 0;
-let availableQuesions = [];
+let availableQuesions = [];//bunu kullanmamim nedeni sorulan sorulari asil datadan siliyoruz ve hic sorulmamis soru gelmesini sagliyoruz
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
-  }
-];
 
-//CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+//CONSTANTS sabit degiskenler
+const CORRECT_BONUS = 10;//dogru sorunun puan degeri
+const MAX_QUESTIONS = 3;//kac soru sorulacak
 
-startGame = () => {
-  questionCounter = 0;
+
+// oyunu baslatiyoruz
+startgame = () => {
   score = 0;
-  availableQuesions = [...questions];
+  questionCounter = 0;//kacinci soruda oldugu
+  availableQuesions = [...questions];//Spread Operator (...)  bir dizi içerisindeki elemanları tek seferde elde etmek ya da bir başka deyişle yazdırmak için Spread operatörü kullanılmaktadır
+  console.log(availableQuesions)
   getNewQuestion();
-};
+}
 
+// yeni soru olusturma kismi
 getNewQuestion = () => {
+
   if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    //go to the end page
-    return window.location.assign("/end.html");
+    localStorage.setItem("mostRecentScore",score)
+    //soru bittiginde yani data kalmadiginda soru sormayi birakip end.html yonlendirme yapiyoruz
+    return window.location.assign("./end.html");
   }
+
+
   questionCounter++;
-// bu kisim soruyu ekrana yazdiriyor
-  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-  currentQuestion = availableQuesions[questionIndex];
+  //questionCounterText.innerText=questionCounter +"/"+MAX_QUESTIONS;//soru sayisini ve hangi soruda oldugunu gosteren gosterge yazisi
+  //questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;////2-2silinen
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;////2-3
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;//3-3
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);//kalan sorulardan birini almak icin
+  currentQuestion = availableQuesions[questionIndex]
   question.innerText = currentQuestion.question;
 
-  
+  //sorunun seceneklerini alip yazdirma kismi
   choices.forEach(choice => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
-
-  availableQuesions.splice(questionIndex, 1);
-  acceptingAnswers = true;
-};
+  availableQuesions.splice(questionIndex, 1);//var olan datadan sorulan sorulari cikartiyoruz
+  acceptingAnswers = true;//sorunun doğru yanlıs sorgulaması yapılması
+}
 
 choices.forEach(choice => {
   choice.addEventListener("click", e => {
     if (!acceptingAnswers) return;
 
     acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset["number"];
+    const selectedChoice = e.target;//tıklanan seceneğ, aktarıyoruz
+    const selectedAnswer = selectedChoice.dataset["number"];//burada dataset ile choise un numarası alınıyor
 
+    //bu kisim secilen sik dogru ise yesil yanlissa kirmizi renk veriyor correct ve incorrecte css yazdim
     const classToApply =
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+
+
+    //2-1 score arttirma
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
 
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
-    }, 1000);
-  });
+    }, 1000);//settimeout koymamin sebebi hemen ekrandan kayboluyor 1 saniye ekranda durum gozukmesi icin
+  })
 });
 
-startGame();
+//2-1 score ekrana yazdirmak icin
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};//score ekrana yazdirmak icin
+
+startgame();
